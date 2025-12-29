b8b13eb5a83b6031fc0883996782470e59086a65
joshbarros <goldenglowitsolutions@gmail.com>
2025-12-29 17:55:48 -0300

    Fix PlayerTools.Tool reference in game_manager.gd

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,1 +1,39 @@
 extends Node
+
+enum Tool {
+    HOE,
+    WATER_BUCKET,
+    SCYTHE,
+    SEED
+}
+
+signal NewDay(day : int)
+signal SetPlayerTool(tool : Tool, seed: CropData)
+signal HarvestCrop(crop : Crop)
+signal ChangeSeedQuantity(crop_data: CropData, quantity : int)
+signal ChangeMoney(money : int)
+
+var day : int = 1
+var money : int = 0
+
+var all_crop_data : Array[CropData] = [
+    preload("res://Crops/tomato.tres"),
+    preload("res://Crops/corn.tres"),
+]
+
+var owned_seeds : Dictionary[CropData, int]
+
+func _ready():
+    pass
+
+func set_next_day():
+    pass
+
+func harvest_crop(crop : Crop):
+    pass
+
+func try_buy_seed(crop_data: CropData):
+    pass
+
+func consume_seed(crop_data : CropData):
+    pass
