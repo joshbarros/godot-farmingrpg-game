b706e47830495a9c0609ebe1d02abfdce1210fc8
joshbarros <goldenglowitsolutions@gmail.com>
2025-12-29 20:50:50 -0300

    Fix node access in ui_manager.gd

--- Scripts/ui_manager.gd
+++ Scripts/ui_manager.gd
@@ -3,11 +3,18 @@ extends CanvasLayer
 var tool_buttons : Array[ToolButton]
 
 func _ready():
-    for child in $ToolButtons.get_children():
-        if child is ToolButton:
-            tool_buttons.append(child)
-    
-    GameManager.SetPlayerTool.connect(_on_set_player_tool)
+    # Get the HBoxContainer that contains the tool buttons
+    var hbox_container = $HBoxContainer
+    if hbox_container:
+        for child in hbox_container.get_children():
+            if child is ToolButton:
+                tool_buttons.append(child)
+
+    # Connect to GameManager if it exists
+    if GameManager:
+        GameManager.SetPlayerTool.connect(_on_set_player_tool)
+    else:
+        print("Warning: GameManager not found, UI manager won't receive tool updates")
 
 func _on_set_player_tool(tool: PlayerTools.Tool, seed: CropData):
     for button in tool_buttons:
